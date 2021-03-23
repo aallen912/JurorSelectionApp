096379bd1818155b6cb390af21c7833243fb215d
Jireh Bethely <54412765+jb24628@users.noreply.github.com>
2021-03-23 16:14:17 -0400

    Add files via upload

---
+++ JurorSelectionApp/JurorSelectionApp/src/main/java/com/JurorSelectionApp/JurorSelectionAppApplication.java
@@ -0,0 +1,13 @@
+package com.JurorSelectionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurorSelectionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurorSelectionAppApplication.class, args);
+	}
+
+}
---
+++ JurorSelectionApp/JurorSelectionApp/src/test/java/com/JurorSelectionApp/JurorSelectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JurorSelectionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurorSelectionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

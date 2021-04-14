f2d814bfb84fa4d4afe8adc0b2e39be786eaf3cb
Jireh Bethely <54412765+jb24628@users.noreply.github.com>
2021-04-14 13:08:27 -0400

    Add files via upload
    Added Lawyer Verification Page
    Added Routing to LV page
    Added LV java class
    Added dependency needed for post calls in LV java file

---
+++ JurorSelectionApp/JurorSelectionApp/src/main/java/com/JurorSelectionApp/HomeController.java
@@ -0,0 +1,21 @@
+package com.JurorSelectionApp;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Version;
+import com.restfb.types.Post;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String test( ) {	
+		return "index.html";
+	}
+}

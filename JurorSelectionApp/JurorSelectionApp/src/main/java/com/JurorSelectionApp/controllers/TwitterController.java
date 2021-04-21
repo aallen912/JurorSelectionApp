01418a95cc0c423f7f4ba128c15fe70be0332439
BlaineHalvorson <blainehalvorson29@gmail.com>
2021-04-20 21:45:07 -0400

    Printing the text and link for a post works

--- JurorSelectionApp/JurorSelectionApp/src/main/java/com/JurorSelectionApp/controllers/TwitterController.java
+++ JurorSelectionApp/JurorSelectionApp/src/main/java/com/JurorSelectionApp/controllers/TwitterController.java
@@ -3,17 +3,12 @@ package com.JurorSelectionApp.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.logging.log4j.util.Chars;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import twitter4j.Paging;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -40,67 +35,56 @@ public class TwitterController {
     twitter.setOAuthConsumer(APIKey, APIKeySecret);
     twitter.setOAuthAccessToken(new AccessToken(AccessToken, AccessTokenSecret));
     
+    //Used for getting every post on a users page
     int pagenum = 1;
+    //Stores all the posts a user has made
     List<Status> FlaggedPosts = new ArrayList<Status>();
-    int numFlagged = 0;
     
-    do{
+    //Filter to store posts that contain key word each post
+    ArrayList<Status> filter = new ArrayList<Status>();
+
+    //Gets every post the user has made and puts it into FlaggedPosts
+    while(true){
       try{
         //Pulling every post from a users page
         int size = FlaggedPosts.size();
         Paging page = new Paging(pagenum++, 100);
         FlaggedPosts.addAll(twitter.getUserTimeline(UserName,page));
-
-        //Filtering each post
-        ArrayList<Status> filter = new ArrayList<Status>();
-        
-        //Searches each status and checks if it contains the keyword, if it doesn't it removes it from Flagged Posts
-        for (Status i : FlaggedPosts) {
-          if(i.getText().contains(Query) == true){
-            filter.add(i);
-            // System.out.println(i.getText()); used for debugging
-          }
-        }
-        
-        //Functionality for showing the post to the user
-        // ArrayList<String> PrintedStatus = new ArrayList();
-        // for (Status status : filter) {
-        //   if(PrintedStatus.contains(status.getText())  == false){
-        //     System.out.println(UserName + " made a post that contained " + Query + " :");
-        //     PrintedStatus.add(status.getText());
-        //     System.out.println(status.getText());
-        //     String url= "https://twitter.com/" + status.getUser().getScreenName() + "/status/" + status.getId();
-        //     System.out.println(url); 
-        //   }else{
-        //     System.out.println("this post was already shown");
-        //   }
-        // }
-        // System.out.println(PrintedStatus.toString());
-
         if(FlaggedPosts.size() == size){
           break;
         }
-        numFlagged = filter.size();
-
-      }catch(TwitterException e){
+        
+        //Sleep functionality to avoid gettting locked out of the twitter api
+        final long duration = System.nanoTime() - startTime;
+        if((5500 - duration / 1000000) > 0){
+          System.out.println("SLEEPING FOR " + (6000 - duration / 1000000) + " miliseconds");
+          try {
+            Thread.sleep((5500 - duration / 1000000));
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+      catch(TwitterException e){
         System.out.println("There was an error finding posts on a users page: ");
         e.printStackTrace();
       }
-      //Sleep functionality to avoid gettting locked out of the twitter api
-      final long duration = System.nanoTime() - startTime;
-      if((5500 - duration / 1000000) > 0){
-        System.out.println("SLEEPING FOR " + (6000 - duration / 1000000) + " miliseconds");
-        try {
-          Thread.sleep((5500 - duration / 1000000));
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
+    }
+    
+    //Searches each status and checks if it contains the keyword, if it does it adds it to filter
+    for (Status i : FlaggedPosts) {
+      if(i.getText().contains(Query) == true){
+        //Adding the post with the keyword to filter
+        filter.add(i);
+        //Stores the text so the user can see the posts in context
+        System.out.println(i.getText());
+        //Constructing the url to show the user
+        String url = "https://twitter.com/" + i.getUser().getScreenName() + "/status/" + i.getId();
+        System.out.println("This post can be viewed here: " + url);
       }
-    }while(true);
-
-    
-    
-    System.out.println(" \n Total Flagged Posts for " + UserName + " for keyword " + Query +": " + numFlagged);
+    }
+    //Showing the user the final results for a username/query combo
+    System.out.println("\nTotal Flagged Posts for" + UserName + "for keyword" + Query +": " + filter.size());
   }
 
 
@@ -109,19 +93,16 @@ public class TwitterController {
   @ResponseBody
   public static void getTwitterStatus(@RequestParam(required = true) String Username, @RequestParam(required = true) String query){
 
+    //Splitting the String received into String arrays that contain each comman delimited word
     String[] ArrayUsername = Username.split(",",0);
     String[] ArrayQuery = query.split(",",0);
 
     //Formatting the strings received to contain one space on either side of them
-    for (String str : ArrayUsername) {
-      String.format("%" + 1 + "s", str);
-      String.format("%" + (-1) + "s", str);
-      //System.out.println(str); Used for debugging
+    for (int i = 0; i < ArrayUsername.length; i++) {
+      ArrayUsername[i] = String.format(" %1s ", ArrayUsername[i]);
     }
-    for (String str : ArrayQuery) {
-      String.format("%" + 1 + "s", str);
-      String.format("%" + (-1) + "s", str);
-      //System.out.println(str); Used for debugging
+    for (int i = 0; i < ArrayQuery.length; i++) {
+      ArrayQuery[i] = String.format(" %1s ", ArrayQuery[i]);
     }
 
     //This loop will run through each keyword given once for each username given
